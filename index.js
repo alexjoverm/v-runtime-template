cea3310c3b0ebaeb6c871e616f444808f5b0365d
Alex J <alexjovermorales@gmail.com>
2018-04-17 13:17:34 +0200

    Fix issue by getting options from parent automatically

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 export default {
   props: {
     template: String,
-    options: {
-      type: Object,
-      default: () => ({}),
-    },
   },
   computed: {
     dynamic() {
-      const { props, data, methods, computed } = this.options;
-      return { template: this.template, props, data, methods, computed };
+      const {
+        props,
+        data,
+        methods,
+        computed,
+        components,
+        watch,
+      } = this.$parent.$options;
+
+      return {
+        template: this.template,
+        props,
+        data,
+        methods,
+        computed,
+        components,
+        watch,
+      };
     },
   },
   render(h) {
