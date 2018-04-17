a4f2c5ec611db3ea48f925bcf2895470e569e3f8
Alex J <alexjovermorales@gmail.com>
2018-04-17 15:23:22 +0200

    Build latest version

--- dist/v-runtime-template.cjs.js
+++ dist/v-runtime-template.cjs.js
@@ -9,19 +9,26 @@
 var index = {
   props: {
     template: String,
-    options: {
-      type: Object,
-      default: function () { return ({}); },
-    },
   },
   computed: {
     dynamic: function dynamic() {
-      var ref = this.options;
+      var ref = this.$parent.$options;
       var props = ref.props;
       var data = ref.data;
       var methods = ref.methods;
       var computed = ref.computed;
-      return { template: this.template, props: props, data: data, methods: methods, computed: computed };
+      var components = ref.components;
+      var watch = ref.watch;
+
+      return {
+        template: this.template,
+        props: props,
+        data: data,
+        methods: methods,
+        computed: computed,
+        components: components,
+        watch: watch,
+      };
     },
   },
   render: function render(h) {
