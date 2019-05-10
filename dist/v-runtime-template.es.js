d54078e26b5934e087641b22b5e89842137b0769
Andrew Hunter <hunterae@gmail.com>
2019-05-09 21:24:27 -0400

    Regenerated build

--- dist/v-runtime-template.es.js
+++ dist/v-runtime-template.es.js
@@ -1,6 +1,6 @@
 /**
- * v-runtime-template v1.6.0
- * (c) 2018 Alex J <alexjovermorales@gmail.com>
+ * v-runtime-template v1.6.2
+ * (c) 2019 Alex J <alexjovermorales@gmail.com>
  * @license MIT
  */
 
@@ -36,14 +36,24 @@ var index = {
 
     if (this.template) {
       var ref = this.$parent;
-      var $data = ref.$data;
-      var $props = ref.$props;
-      var $options = ref.$options;
-      var components = $options.components;
-      var computed = $options.computed;
-      var methods = $options.methods;
+      var $data = ref.$data; if ( $data === void 0 ) $data = {};
+      var $props = ref.$props; if ( $props === void 0 ) $props = {};
+      var $options = ref.$options; if ( $options === void 0 ) $options = {};
+      var components = $options.components; if ( components === void 0 ) components = {};
+      var computed = $options.computed; if ( computed === void 0 ) computed = {};
+      var methods = $options.methods; if ( methods === void 0 ) methods = {};
 
       var passthrough = {$data:{}, $props:{}, $options:{}, components:{}, computed:{}, methods:{}};
+
+      if (typeof this.$options.methods === "undefined") {
+        this.$options.methods = {};
+      }
+      if (typeof this.$options.computed === "undefined") {
+        this.$options.computed = {};
+      }
+      if (typeof this.$options.components === "undefined") {
+        this.$options.components = {};
+      }
 
       //build new objects by removing keys if already exists (e.g. created by mixins)
       Object.keys($data).forEach(function (e) {if(typeof this$1.$data[e]==="undefined") { passthrough.$data[e] = $data[e]; }} );
