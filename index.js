2c1ec706ef07d419be27d81b8eb517827f66486e
Alex J <alexjovermorales@gmail.com>
2019-06-28 16:11:26 +0200

    fix: default objects to avoid crashing

--- index.js
+++ index.js
@@ -28,28 +28,62 @@ export default {
   },
   render(h) {
     if (this.template) {
-      const { $data = {}, $props = {}, $options = {} } = this.$parent;
-      const { components = {}, computed = {}, methods = {} } = $options;
-
-      let passthrough = {$data:{}, $props:{}, $options:{}, components:{}, computed:{}, methods:{}};
-      this.$options = this.$options || {}
-      this.$options.methods = this.$options.methods || {}
-      this.$options.computed = this.$options.computed || {}
-      this.$options.components = this.$options.computed || {}
+      const {
+        $data: parentData = {},
+        $props: parentProps = {},
+        $options: parentOptions = {}
+      } = this.$parent;
+      const {
+        components: parentComponents = {},
+        computed: parentComputed = {},
+        methods: parentMethods = {}
+      } = parentOptions;
+      const {
+        $data = {},
+        $props = {},
+        $options: { methods = {}, computed = {}, components = {} } = {}
+      } = this;
+      const passthrough = {
+        $data: {},
+        $props: {},
+        $options: {},
+        components: {},
+        computed: {},
+        methods: {}
+      };
 
       //build new objects by removing keys if already exists (e.g. created by mixins)
-      Object.keys($data).forEach(e => {if(typeof this.$data[e]==="undefined") passthrough.$data[e] = $data[e];} );
-      Object.keys($props).forEach(e => {if(typeof this.$props[e]==="undefined") passthrough.$props[e] = $props[e];} );
-      Object.keys(methods).forEach(e => {if(typeof this.$options.methods[e]==="undefined") passthrough.methods[e] = methods[e];} );
-      Object.keys(computed).forEach(e => {if(typeof this.$options.computed[e]==="undefined") passthrough.computed[e] = computed[e];} );
-      Object.keys(components).forEach(e => {if(typeof this.$options.components[e]==="undefined") passthrough.components[e] = components[e];} );
+      Object.keys(parentData).forEach(e => {
+        if (typeof $data[e] === "undefined")
+          passthrough.$data[e] = parentData[e];
+      });
+      Object.keys(parentProps).forEach(e => {
+        if (typeof $props[e] === "undefined")
+          passthrough.$props[e] = parentProps[e];
+      });
+      Object.keys(parentMethods).forEach(e => {
+        if (typeof methods[e] === "undefined")
+          passthrough.methods[e] = parentMethods[e];
+      });
+      Object.keys(parentComputed).forEach(e => {
+        if (typeof computed[e] === "undefined")
+          passthrough.computed[e] = parentComputed[e];
+      });
+      Object.keys(parentComponents).forEach(e => {
+        if (typeof components[e] === "undefined")
+          passthrough.components[e] = parentComponents[e];
+      });
 
-      const methodKeys = Object.keys(passthrough.methods || [])
+      const methodKeys = Object.keys(passthrough.methods || []);
       const dataKeys = Object.keys(passthrough.$data || {});
       const propKeys = Object.keys(passthrough.$props || {});
       const allKeys = dataKeys.concat(propKeys).concat(methodKeys);
       const methodsFromProps = buildFromProps(this.$parent, methodKeys);
-      const props = merge([passthrough.$data, passthrough.$props, methodsFromProps]);
+      const finalProps = merge([
+        passthrough.$data,
+        passthrough.$props,
+        methodsFromProps
+      ]);
 
       const dynamic = {
         template: this.template || "<div></div>",
@@ -58,9 +92,7 @@ export default {
         components: passthrough.components
       };
 
-      return h(dynamic, {
-        props
-      });
+      return h(dynamic, { props: finalProps });
     }
   }
 };
